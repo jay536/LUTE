c181d7170bc97ece2ac03171e506ea1bbb5a260a
jay536 <s5309473@bournemouth.ac.uk>
2025-01-29 02:32:57 +0000

    more inventory things

--- Assets/me/scripts/setactives.cs
+++ Assets/me/scripts/setactives.cs
@@ -6,10 +6,19 @@ namespace LoGaCulture.LUTE
     {
 
         public GameObject dressup;
+
+        public GameObject collected;
+
+
+        public GameObject character1;
+        public GameObject character2;
+        public GameObject charater3;
+        public GameObject charater4;
+        //public float collectedNum;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            //collectedNum = 0;
         }
 
         // Update is called once per frame
@@ -21,6 +30,17 @@ namespace LoGaCulture.LUTE
         public void dressupon()
         {
             dressup.SetActive(true);
+            character1.SetActive(true);
+        }
+
+        public void opencollected()
+        {
+
+         collected.SetActive(true);
+        }
+        public void closecollected()
+        {
+         collected.SetActive(false);
         }
     }
 }
