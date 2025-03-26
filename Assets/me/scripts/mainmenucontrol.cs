ea225fb0c0d4b174f14c8c144bcc1c30c2be56fc
jay536 <s5309473@bournemouth.ac.uk>
2025-03-26 17:35:48 +0000

    small changes

--- Assets/me/scripts/mainmenucontrol.cs
+++ Assets/me/scripts/mainmenucontrol.cs
@@ -5,11 +5,24 @@ namespace LoGaCulture.LUTE
     public class mainmenucontrol : MonoBehaviour
     {
 
+        public GameObject fullmenu;
+
         public GameObject menu;
+
+        public GameObject credits;
+
+        public GameObject splash;
+
+        //public GameObject luteui;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            splash.SetActive(true);
+            menu.SetActive(false);
+            credits.SetActive(false);
+           // luteui = GameObject.Find("PopupIcon");
+
+           // luteui.SetActive(false);
         }
 
         // Update is called once per frame
@@ -20,7 +33,26 @@ namespace LoGaCulture.LUTE
 
         public void start()
         {
+            fullmenu.SetActive(false);
+           // luteui.SetActive(true);
+        }
+
+        public void closesplash()
+        {
+            splash.SetActive(false);
+            menu.SetActive(true);
+        }
+
+        public void opencredits()
+        {
+            credits.SetActive(true);
             menu.SetActive(false);
+        }
+
+        public void closecredits()
+        {
+            credits.SetActive(false);
+            menu.SetActive(true);
         }
     }
 }
