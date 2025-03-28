7fd5a86a4b72dba9b12a4c756513067484ad36a9
jay536 <s5309473@bournemouth.ac.uk>
2025-03-28 13:23:15 +0000

    ui and timer

--- Assets/me/scripts/FloatVariableUI.cs
+++ Assets/me/scripts/FloatVariableUI.cs
@@ -17,6 +17,10 @@ namespace LoGaCulture.LUTE
         [VariableProperty(typeof(BooleanVariable))]
         [SerializeField] protected BooleanVariable showUI;
 
+        [SerializeField] protected GameObject gameui;
+
+        //public float time;
+
         private void Update()
         {
             if (textComponent != null && floatVariable != null && showUI != null)
@@ -25,19 +29,62 @@ namespace LoGaCulture.LUTE
                 if (showUI.Value == false)
                 {
                     textComponent.enabled = true;
+                    gameui.SetActive(true);
+
+                    /* int minutes = floatVariable.Value;
+                     int hours = minutes / 60;
+                     int mins = minutes % 60;
+
+                     string timeFormat = $"{hours:D2}:{mins:D2}";*/
                 }
                 else
                 {
                     // Once we can click, hide the time left
                     // You could also show something here to inform the player they can now click
                     textComponent.enabled = false;
+                    gameui.SetActive(false);
                 }
 
                 textComponent.text = floatVariable.Value.ToString();
+
+
+
+                /* if (time > 0)
+                 {
+                     time -= Time.deltaTime;
+                 }
+                 else if (time < 0)
+                 {
+                     time = 0;
+                 }
+                 int minutes = Mathf.FloorToInt(time / 60);
+                 int seconds = Mathf.FloorToInt(time % 60);
+                 textComponent.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+                 time = float.Parse(textComponent.text);
+                 //time = float.TryParse(floatVariable);*/
+                int minutes = floatVariable.Value;
+                int hours = minutes / 60;
+                int mins = minutes % 60;
+
+                string timeFormat = $"{hours:D2}:{mins:D2}";
+                textComponent.text = timeFormat;
             }
             // If we cannot find the text component or relevant variables then disable it
+
+
             else
                 textComponent.enabled = false;
+
+            
         }
+
+        /*private void Start()
+        {
+
+            {
+                time = floatVariable.Value;
+            }
+        }*/
     }
 }
